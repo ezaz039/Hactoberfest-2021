8c1de6fce2c8a24da449c4416936819edd41118c
Soumikpaul <44001521+PaulSoumik@users.noreply.github.com>
2021-10-05 23:32:12 +0530

    Added Comment

--- valid_paranthesis.cpp
+++ valid_paranthesis.cpp
@@ -10,12 +10,12 @@ public:
 
         for (auto it {s.begin()}; it != s.end(); it++) {
             if (brackets.empty() || brackets.top() != brackets_pair[*it])
-                brackets.push(*it);   
+                brackets.push(*it); //push new bracket to the stack
             else if (!brackets.empty() && brackets.top() == brackets_pair[*it]) 
-                brackets.pop();
+                brackets.pop(); //remove the bracket from the stack if pair found
             else return false;
         }
         
-        return brackets.empty();
+        return brackets.empty(); //if statck size > 0 means there are brackets without a pair
     }
 };
